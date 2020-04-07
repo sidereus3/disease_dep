92c3deb38070cc91ddcbadd33a094e5cc1163766
sidereus <francesco.serafin.3@gmail.com>
2020-04-07 12:57:58 -0600

    fix: removed Rserve

--- install.R
+++ install.R
@@ -1,7 +1,4 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("stringi","foreach",
-                   "doParallel","raster","rgdal",
-                   "sp","Rcpp"),
-repos="http://lib.stat.cmu.edu/R/CRAN")
-install.packages("Rserve", "Rserve_1.8-6.tgz", "http://www.rforge.net/", lib="/tmp2/Rlibs/")
+install.packages(c("stringi","foreach", "doParallel","raster","rgdal",
+                   "sp","Rcpp"), repos="http://lib.stat.cmu.edu/R/CRAN")
