7e86991e3f5649fa72a250db8d64f2b24e21db84
sidereus <francesco.serafin.3@gmail.com>
2020-04-07 11:49:52 -0600

    fix: Rserve 1.8-6

--- install.R
+++ install.R
@@ -2,5 +2,6 @@
 
 install.packages(c("stringi","foreach",
                    "doParallel","raster","rgdal",
-                   "sp","Rserve","Rcpp"),
+                   "sp","Rcpp"),
 repos="http://lib.stat.cmu.edu/R/CRAN")
+install.packages("Rserve", "Rserve_1.8-6.tgz", "http://www.rforge.net/")
